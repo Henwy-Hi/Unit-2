2458f596e3d9b37cd9fd266d3e8a08c371307a3f
Henry <felstedh@wlhs.wlwv.k12.or.us>
2024-10-16 20:43:50 -0700

    Finished the project!

--- src/word_games/WordGames.java
+++ src/word_games/WordGames.java
@@ -21,12 +21,12 @@ public class WordGames
      */
     public String scramble(String word)
     {
+        int halfLength = (word.length() / 2);
         // switch first half
-        String firstHalf = (word.substring(0, (word.length()/2)));
+        String firstHalf = (word.substring(0, halfLength));
         // and second half
-        String secondHalf = (word.substring((word.length()/2), word.length()));
-        return secondHalf + firstHalf;
-
+        String secondHalf = (word.substring(halfLength, word.length()));
+        return (secondHalf + firstHalf);
     }
 
     /**
@@ -38,8 +38,10 @@ public class WordGames
     public String bananaSplit(int insertIdx, String insertText)
     {
         // Insert insertText at the position
+        String firstHalf = word.substring(0, insertIdx);
+        String secondHalf = word.substring(insertIdx, word.length());
         // insertIdx
-        return "";
+        return (firstHalf + insertText + secondHalf);
 
     }
 
@@ -50,11 +52,14 @@ public class WordGames
      * @param insertText String
      * @return the modified text
      */
-    public String bananaSplit(String insertChar, String insertText)
+    public String bananaSplit(char insertChar, String insertText)
     {
+        int index = word.indexOf(insertChar);
         // Insert insertText after the first
+        String firstHalf = word.substring(0, index + 1);
+        String secondHalf = word.substring(index + 1);
         // occurence of the insertChar
-        return "";
+        return firstHalf + insertText + secondHalf;
 
     }
 
--- src/word_games/WordGamesTester.java
+++ src/word_games/WordGamesTester.java
@@ -6,26 +6,36 @@ public class WordGamesTester {
         Scanner input = new Scanner(System.in);
 
         // Ask for a word
-
+        System.out.println("Enter a word: ");
+        String word = input.nextLine();
         // Scramble it
         // Print out scrambled word
-
+        WordGames wordGames = new WordGames(word);
+        word = wordGames.scramble(word);
+        System.out.println(word);
         // Ask for an index
-        // Ask for random word
         System.out.println("Enter an integer: ");
         int idx = input.nextInt();
+        // Ask for random word
         input.nextLine();  // skip over the newline
         System.out.println("Enter another word: ");
-        String word = input.nextLine();
-
+        String nextWord = input.nextLine();
         // Add random word at index
+        nextWord = wordGames.bananaSplit(idx, nextWord);
+        System.out.println(nextWord);
         // Print out the word
 
 
         // Ask for a character (store as a String)
+        System.out.println("Enter a character: ");
+        char ch = input.next().charAt(0);
+        input.nextLine();
         // Ask for random word
+        System.out.println("Enter yet another random word: ");
+        String finalWord = input.nextLine();
         // Add random word at character
+        finalWord = wordGames.bananaSplit(ch, finalWord);
         // Print out the word
-
+        System.out.println(finalWord);
     }
 }
